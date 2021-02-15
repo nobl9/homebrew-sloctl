98bebc03138a4fda397fc7d47b7e1e117292106a
nobl9 <greg@nobl9.com>
2021-02-15 11:01:27 +0000

    Version 0.0.43

--- Formula/sloctl.rb
+++ Formula/sloctl.rb
@@ -1,15 +1,15 @@
 class Sloctl < Formula
     desc "Command-line client for Nobl9"
     homepage "https://nobl9.com"
-    version ""
+    version "0.0.43"
     bottle :unneeded
   
     if OS.mac?
-      url "https://github.com/nobl9/sloctl/releases/download//sloctl-macos-.zip"
-      sha256 ""
+      url "https://github.com/nobl9/sloctl/releases/download/0.0.43/sloctl-macos-0.0.43.zip"
+      sha256 "ab6171eb2b60dfb4173a49f7e510a49749278a80bb91f9910606a814369a4288  sloctl-macos-0.0.43.zip"
     elsif OS.linux?
-      url "https://github.com/nobl9/sloctl/releases/download//sloctl-linux-.zip"
-      sha256 ""
+      url "https://github.com/nobl9/sloctl/releases/download/0.0.43/sloctl-linux-0.0.43.zip"
+      sha256 "9e6d78523f562b3bfc8c97799bcdb67f1f8c815d5704036796c3f0d3694996b2  sloctl-linux-0.0.43.zip"
     end
   
     def install
